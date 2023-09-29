f8e21c3c90f12e53e1db57b85b7620f134f43ce4
Roberto Arruda <roberto0arruda@hotmail.com>
2023-09-29 10:01:24 -0400

    ci: fix running stress test
    Signed-off-by: Roberto Arruda <roberto0arruda@hotmail.com>

--- docker/db.sql
+++ docker/db.sql
@@ -1,8 +1,13 @@
 
-create table if not exists pessoas(
-  id uuid not null primary key,
-  apelido varchar(64) not null unique,
-  nome varchar(200) not null,
-  nascimento timestamp not null,
-  stack VARCHAR[] null default '{}',
+CREATE TABLE IF NOT EXISTS public.pessoas
+(
+    id         uuid         NOT NULL,
+    apelido    varchar(32)  NOT NULL,
+    nome       varchar(100) NOT NULL,
+    nascimento date         NOT NULL,
+    stack      json NULL,
+    created_at timestamp(0) NULL,
+    updated_at timestamp(0) NULL,
+    CONSTRAINT pessoas_apelido_unique UNIQUE (apelido),
+    CONSTRAINT pessoas_pkey PRIMARY KEY (id)
 );
